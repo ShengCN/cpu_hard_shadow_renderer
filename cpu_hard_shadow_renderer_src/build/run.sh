7b3c528838228e4abb610cc3e9a0b8552a0bf5d7
ShengCN <451718145@qq.com>
2020-11-09 22:25:43 -0500

    update script

--- cpu_hard_shadow_renderer_src/build/run.sh
+++ cpu_hard_shadow_renderer_src/build/run.sh
@@ -1,3 +1,3 @@
 make -j8
 sudo rm airplane/*shadow.png
-sudo nvprof ./hard_shadow --model="/home/ysheng/Dataset/human_models/simulated_combine_male_short_outfits_genesis8_armani_sweateroutfitall_Base_Pose_Walking_B.obj" --output="airplane" --cam_pitch=30 --model_rot=-45 --render_touch --render_mask
+sudo nvprof ./hard_shadow --model="/home/ysheng/Dataset/benchmark_ds/models/general/bottle_0344.off" --output="airplane" --cam_pitch=30 --model_rot=-45 --render_touch --render_mask
